9c19dd92f54680b7a815aace2205c164145c4785
Thomas A. Valletta <github.tom@vallettas.com>
2011-12-01 10:49:18 -0700

    Everything seems to work

--- js/display-connection.js
+++ js/display-connection.js
@@ -1,13 +1,13 @@
-var socket,
-	server = 'http://127.0.0.1:3009';
+var socket2,
+	server2 = 'http://127.0.0.1:3009';
 
 var connect = function(callback) {
-	console.log("Attempting to connect to " + server);
-	socket = io.connect(server);
-	socket.on('ack', function() {
+	console.log("Attempting to connect to " + server2);
+	socket2 = io.connect(server2);
+	socket2.on('ack', function() {
 		if (callback) callback();
 	});
-	socket.on('score', function(totals) {
+	socket2.on('score', function(totals) {
 		var val = totals.pos - totals.neg;
 		var max = totals.cnt * 2;
 		var pos = (((val / max) * 100) << 0);
--- js/remote.js
+++ js/remote.js
@@ -4,6 +4,7 @@ var socket = io.connect('http://127.0.0.1:3001');
 
 if (window.location.href.indexOf("remote") > 0) {
     document.addEventListener("slideenter", function(evt) {
+		console.log('sliding');
         socket.emit('slideto', {slideNumber: evt.slideNumber});
     });
 }
